d5020a40a45ac9c7eedab0a2fc855c6d654d326c
Nerelis Fernandez <nerelis@Nereliss-MBP.lan>
2024-06-04 23:42:52 -0400

    Initial commit

---
+++ SerenityJournal/SerenityJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerenityJournalApp.swift
+//  SerenityJournal
+//
+//  Created by Nerelis Fernandez on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SerenityJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
